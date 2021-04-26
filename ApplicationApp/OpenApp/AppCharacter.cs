309707c215b16633c07ba365d24f066bce9e0903
Marcelo Cardoso Feiteiro <mfeiteiro@hotmail.com>
2021-04-26 13:31:26 -0300

    criacao de uma versao da tea de loading

--- ApplicationApp/OpenApp/AppCharacter.cs
+++ ApplicationApp/OpenApp/AppCharacter.cs
@@ -1,6 +1,32 @@
-﻿namespace ApplicationApp.OpenApp
+﻿using ApplicationApp.Interfaces;
+using Domain.Interfaces.InterfacesEntities;
+using Entities;
+using System.Threading.Tasks;
+
+namespace ApplicationApp.OpenApp
 {
-    public class AppCharacter
+    public class AppCharacter : IAppCharacter
     {
+        private readonly ICharacter _character;
+
+        public Task Add(Character obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(Character obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Character> GetEntityById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Update(Character obj)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
